6c628160ed96b104e2a0eff0521b13ff58768ac2
Harshal Dandade <terminaldt20@gmail.com>
2026-02-22 00:18:46 +0530

    disabling CSRF

--- security/src/main/java/com/example/security/ProController.java
+++ security/src/main/java/com/example/security/ProController.java
@@ -1,4 +1,12 @@
 package com.example.security;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProController {
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello World!!" ;
+    }
 }
